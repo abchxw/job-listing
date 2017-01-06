6b8a47130f1eeeb070925e1c6e1b064ea2a49449
hexiuwei <abchxw@gmail.com>
2017-01-07 02:54:38 +0800

    part3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106153748) do
+ActiveRecord::Schema.define(version: 20170106185113) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170106153748) do
     t.datetime "updated_at",       null: false
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
-    t.string   "contact_mail"
+    t.string   "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
